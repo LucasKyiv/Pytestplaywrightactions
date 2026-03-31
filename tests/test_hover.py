08d809830512e81071ccaca5143eb0ca6aa8a182
Ihor Nazarkin <ihornazarkin@gmail.com>
2026-03-31 22:55:55 +0100

    more tests added

--- tests/test_hover.py
+++ tests/test_hover.py
@@ -33,9 +33,8 @@ def test_select_element_after_hover(page: Page) -> None:
 
     page.wait_for_timeout(3000)
     all_free_ebooks = page.locator("//li[@id='menu-item-7128']//ul[@class='sub-menu']//a")
-    print(all_free_ebooks.all_inner_texts())
+    texts = all_free_ebooks.all_inner_texts()
+    assert texts[1] == "Free Deep Learning eBooks", f"Unexpected second item: {texts[1]}"
 
     free_deep_learning_book = page.locator(get_header_submenu_loc("Free Ebooks", "Tensorflow"))
     free_deep_learning_book.click()
-    page.wait_for_timeout(10000)
-    print(page.title())
