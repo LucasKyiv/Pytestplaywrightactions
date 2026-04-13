0f5f5cf3ec826e4b985cc0f19537289370eeb4dc
Ihor Nazarkin <ihornazarkin@gmail.com>
2026-04-13 13:30:22 +0100

    commiting popup testssdf

--- tests/test_some_editable_fields.py
+++ tests/test_some_editable_fields.py
@@ -3,11 +3,17 @@ from playwright.sync_api import Page, expect
 
 def test_field_editable(page: Page) -> None:
     page.goto("https://jsfiddle.net/L96svw3c")
-    editable_field = page.frame("result").locator("#readonly")
+
+    frame = page.frame_locator("iframe[name='result']")
+    editable_field = frame.locator("#readonly")
+
     expect(editable_field).to_be_editable()
 
 
 def test_read_only_field(page: Page) -> None:
     page.goto("https://jsfiddle.net/L96svw3c")
-    read_only_field = page.frame("result").locator("#readOnly")
+
+    frame = page.frame_locator("iframe[name='result']")
+    read_only_field = frame.locator("#readOnly")
+
     expect(read_only_field).not_to_be_editable()
